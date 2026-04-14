a033947ecd49eb19b9c889f6e812d54043621c9e
Sumally007 <ismailmbarouk007@gmail.com>
2026-04-14 20:48:11 +0300

    Optimization of Categories section

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -10,19 +10,19 @@ import socksImg from '../assets/images/Rectangle 11 (1).png';
 import sweatPantsImg from '../assets/images/Rectangle 12 (1).png';
 
 
-const categoriesData = [
-    { id: 1, title: 'Tops', image: topsImg },
-    { id: 2, title: 'Dress', image: dressImg },
-    { id: 3, title: 'Shoes', image: shoesImg },
-    { id: 4, title: 'Hats', image: hatsImg },
-    { id: 5, title: 'T-shirt', image: tshirtImg },
-    { id: 6, title: 'Pants', image: pantsImg },
-    { id: 7, title: 'Socks', image: socksImg },
-    { id: 8, title: 'Sweat Pants', image: sweatPantsImg },
-];
-
-
 const Categories = () => {
+
+    const categoriesData = [
+        { id: 1, title: 'Tops', image: topsImg },
+        { id: 2, title: 'Dress', image: dressImg },
+        { id: 3, title: 'Shoes', image: shoesImg },
+        { id: 4, title: 'Hats', image: hatsImg },
+        { id: 5, title: 'T-shirt', image: tshirtImg },
+        { id: 6, title: 'Pants', image: pantsImg },
+        { id: 7, title: 'Socks', image: socksImg },
+        { id: 8, title: 'Sweat Pants', image: sweatPantsImg },
+    ];
+
     return (
         <section className="category-section">
             <div className="category-header">
@@ -32,16 +32,22 @@ const Categories = () => {
 
             <div className="category-grid">
                 {categoriesData.map((item) => (
-                    <div key={item.id} className="category-card">
-                        <div className="card-image-wrapper">
-                            <img src={item.image} alt={item.title} />
-                        </div>
-                        <h3>{item.title}</h3>
-                    </div>
+                    <CategoryCard key={item.id} item={item} />
                 ))}
             </div>
         </section>
     )
 }
+
+const CategoryCard = ({ item }) => {
+    return (
+        <div className="category-card">
+            <div className="card-image-wrapper">
+                <img src={item.image} alt={item.title} />
+            </div>
+            <h3>{item.title}</h3>
+        </div>
+    );
+};
 
 export default Categories
