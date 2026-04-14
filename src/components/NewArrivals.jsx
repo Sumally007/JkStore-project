3167785bca67ba10b380cf7095ce2782cadd0869
Sumally007 <ismailmbarouk007@gmail.com>
2026-04-14 21:06:34 +0300

    Optimization of NewArrivals section

--- src/components/NewArrivals.jsx
+++ src/components/NewArrivals.jsx
@@ -4,42 +4,41 @@ import Dressing1 from "../assets/images/Rectangle 16.png"
 import Dressing2 from "../assets/images/Rectangle 17.png"
 
 const NewArrivals = () => {
+
+    const NewArrivalsData = [
+        { id: 1, title: 'Knitwear Hijab Dress', subtitle: 'Buy Now', image: Dressing },
+        { id: 2, title: 'Shirt/blouse', subtitle: 'Buy Now', image: Dressing1 },
+        { id: 3, title: 'Sweatshirt', subtitle: 'Buy Now', image: Dressing2 },
+    ];
+
+
     return (
         <section className='new-arrivals-container'>
             <div className="new-arrivals-contents">
                 <h1 className='title'>2022 New Arrivals!</h1>
                 <div className="card-container">
-                    <div className="card">
-                        <div className="image-card">
-                            <img src={Dressing} alt="Dressing" />
-                        </div>
-                        <div className="text-card">
-                            <p className='text'>Knitwear Hijab Dress</p>
-                            <a className='link' href="#">Buy Now</a>
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="image-card">
-                            <img src={Dressing1} alt="Dressing1" />
-                        </div>
-                        <div className="text-card">
-                            <p className='text'>Shirt/blouse</p>
-                            <a className='link' href="#">Buy Now</a>
-                        </div>
-                    </div>
-                    <div className="card">
-                        <div className="image-card">
-                            <img src={Dressing2} alt="Dressing2" />
-                        </div>
-                        <div className="text-card">
-                            <p className='text'>Sweatshirt</p>
-                            <a className='link' href="#">Buy Now</a>
-                        </div>
-                    </div>
+                    {NewArrivalsData.map((item) => (
+                        <Card key={item.id} item={item} />
+                    ))}
                 </div>
 
             </div>
         </section>
+    )
+}
+
+const Card = ({ item }) => {
+    console.log(item)
+    return (
+        <div className="card">
+            <div className="image-card">
+                <img src={item.image} alt={item.image} />
+            </div>
+            <div className="text-card">
+                <p className='text'>{item.title}</p>
+                <a className='link' href="#">{item.subtitle}</a>
+            </div>
+        </div>
     )
 }
 
