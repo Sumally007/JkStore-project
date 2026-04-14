9d726fa18c49b2f0bb46cfd4f479fd7c1f831661
Sumally007 <ismailmbarouk007@gmail.com>
2026-04-14 13:47:09 +0300

    About section done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar';
 import Hero from './components/Hero';
 import Features from './components/Features';
 import NewArrivals from './components/NewArrivals';
+import About from './components/About';
 
 const App = () => {
   return (<>
@@ -11,6 +12,7 @@ const App = () => {
     <Hero />
     <Features />
     <NewArrivals />
+    <About />
   </>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import patternImage from "../assets/images/Rectangle 19.png"
+
+const About = () => {
+    return (
+        <section className="about-container">
+            <div className="about-image">
+                <img src={patternImage} alt="Geometric Pattern" />
+            </div>
+
+            <div className="about-content">
+                <h2 className="about-title">About Us</h2>
+                <p className="about-text">
+                    Are you looking for affordable and high-quality clothing in Tanzania?
+                    Look no further, JK Store is the answer.
+                </p>
+                <p className="about-subtext">
+                    JK Store provides Tanzanian women with high-quality modest clothes
+                    at an affordable price. All our clothes selections are sourced from Turkey.
+                </p>
+                <button className="about-btn">Contact Us</button>
+            </div>
+        </section>
+    )
+}
+
+export default About
